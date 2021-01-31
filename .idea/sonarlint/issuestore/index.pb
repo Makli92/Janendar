7b224ed604f4c61f11747f095589a2fafec002cf
Kostandin Tahiri <kostandin.tahiri@klarna.com>
2021-01-31 19:32:15 +0100

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+f
+6app/src/main/java/com/example/janendar/MainActivity.kt,e/7/e7c91a718547a9cc1f5dfbf5c1829876a93ffdc3
+x
+Happ/src/androidTest/java/com/example/janendar/ExampleInstrumentedTest.kt,1/4/148736746fb69a97cfa1ff25c9490f600c176067
---
+++ app/src/main/java/com/example/janendar/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.janendar
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        calendar_view.setOnDateChangeListener { _, day, month, year ->
+            Toast.makeText(
+                    this@MainActivity,
+                    "The selected date is $year.${month + 1}.$day",
+                    Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        calendar_view.firstDayOfWeek = 1
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
